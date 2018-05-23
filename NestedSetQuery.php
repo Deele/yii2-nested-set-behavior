1c8940cd84e69ac6c0340676eb2d4f83a0aeace9
Nils <deele@tuta.io>
2018-05-23 14:08:23 +0300

    Fix formatting and docblocks

--- NestedSetQuery.php
+++ NestedSetQuery.php
@@ -3,22 +3,29 @@
  * @link https://github.com/wbraganca/yii2-nested-set-behavior
  * @copyright Copyright (c) 2014 Wanderson Bragança
  * @license http://opensource.org/licenses/BSD-3-Clause
+ *
+ * @author Wanderson Bragança (wbraganca)
+ * @author Nils (Deele) <deele@tuta.io>
  */
 
 namespace wbraganca\behaviors;
 
-use wbraganca\behaviors\NestedSetQueryBehavior;
+use yii\db\ActiveQuery;
 
 /**
  * @author Wanderson Bragança <wanderson.wbc@gmail.com>
  */
-class NestedSetQuery extends \yii\db\ActiveQuery
+class NestedSetQuery extends ActiveQuery
 {
+
+    /**
+     * @inheritdoc
+     */
     public function behaviors()
     {
         return [
             [
-                'class' => NestedSetQueryBehavior::className(),
+                'class' => NestedSetQueryBehavior::class,
             ]
         ];
     }
