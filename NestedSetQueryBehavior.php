307a4e2db929875ec7fb9fe29b0a07a1a1cc1905
Nils <deele@tuta.io>
2018-05-23 14:05:31 +0300

    Enhance options(), fix formatting and docblocks

--- NestedSetQueryBehavior.php
+++ NestedSetQueryBehavior.php
@@ -3,11 +3,17 @@
  * @link https://github.com/wbraganca/yii2-nested-set-behavior
  * @copyright Copyright (c) 2014 Wanderson Bragança
  * @license http://opensource.org/licenses/BSD-3-Clause
+ *
+ * @author Wanderson Bragança (wbraganca)
+ * @author Nils (Deele) <deele@tuta.io>
  */
 
 namespace wbraganca\behaviors;
 
 use yii\base\Behavior;
+use yii\db\ActiveQuery;
+use yii\db\ActiveRecord;
+use yii\helpers\ArrayHelper;
 
 /**
  * @author Wanderson Bragança <wanderson.wbc@gmail.com>
@@ -21,93 +27,247 @@ class NestedSetQueryBehavior extends Behavior
 
     /**
      * Gets root node(s).
-     * @return ActiveRecord the owner.
+     *
+     * @return ActiveQuery|ActiveRecord
      */
     public function roots()
     {
         /** @var $modelClass ActiveRecord */
         $modelClass = $this->owner->modelClass;
         $model = new $modelClass;
-        $this->owner->andWhere($modelClass::getDb()->quoteColumnName($model->leftAttribute) . '=1');
+        $this->owner->andWhere(
+            $modelClass::getDb()
+                       ->quoteColumnName($model->leftAttribute) . '=1'
+        );
         unset($model);
         return $this->owner;
     }
 
-    public function options($root = 0, $level = null)
-    {
+    /**
+     * @param array $settings
+     *      - int|NestedSetTrait root
+     *      - null level
+     *      - null|callable queryCallback
+     *      - int idAttribute
+     *      - int titleAttribute
+     *      - int subtractLevel
+     *      - string spacer
+     *      - string spacerArrow
+     *
+     * @return array
+     */
+    public function options($settings = []) {
+        $root = ArrayHelper::getValue(
+            $settings,
+            'root',
+            0
+        );
+        $level = ArrayHelper::getValue(
+            $settings,
+            'level',
+            null
+        );
+        $queryCallback = ArrayHelper::getValue(
+            $settings,
+            'queryCallback',
+            null
+        );
+        $titleCallback = ArrayHelper::getValue(
+            $settings,
+            'titleCallback',
+            null
+        );
+        $idAttribute = ArrayHelper::getValue(
+            $settings,
+            'idAttribute',
+            (is_object($root) ? $root->idAttribute : null)
+        );
+        $titleAttribute = ArrayHelper::getValue(
+            $settings,
+            'titleAttribute',
+            (is_object($root) ? $root->titleAttribute : null)
+        );
+        $subtractLevel = ArrayHelper::getValue(
+            $settings,
+            'subtractLevel',
+            0
+        );
+        $spacer = ArrayHelper::getValue(
+            $settings,
+            'spacer',
+            '—'
+        );
+        $spacerArrow = ArrayHelper::getValue(
+            $settings,
+            'spacerArrow',
+            '›'
+        );
         $res = [];
         if (is_object($root)) {
-            $res[$root->{$root->idAttribute}] = str_repeat('—', $root->{$root->levelAttribute} - 1) 
-                . ((($root->{$root->levelAttribute}) > 1) ? '›': '')
-                . $root->{$root->titleAttribute};
 
-            if ($level) {
-                foreach ($root->children()->all() as $childRoot) {
-                    $res += $this->options($childRoot, $level - 1);
+            /**
+             * @var NestedSetBehavior $root
+             */
+            $spacerLevel = $root->{$root->levelAttribute} - 1 - $subtractLevel;
+            if ($spacerLevel >= 0) {
+                $title = str_repeat(
+                    $spacer,
+                    $spacerLevel
+                );
+                if (($root->{$root->levelAttribute} - $subtractLevel) > 1) {
+                    $title .= $spacerArrow;
                 }
-            } elseif (is_null($level)) {
-                foreach ($root->children()->all() as $childRoot) {
-                    $res += $this->options($childRoot, null);
+                if (is_callable($titleCallback)) {
+                    $title = call_user_func(
+                        $titleCallback,
+                        $title,
+                        $root
+                    );
                 }
+                else {
+                    $title .= $root->{$titleAttribute};
+                }
+                $res[$root->{$idAttribute}] = $title;
+            }
+
+            $query = $root->children();
+            if (is_callable($queryCallback)) {
+                $query = call_user_func(
+                    $queryCallback,
+                    $query
+                );
+            }
+            foreach ($query->all() as $childRoot) {
+                $settings['root'] = $childRoot;
+                $settings['level'] = (!is_null($level) ? $level - 1 : $level);
+                $res += $this->options($settings);
             }
         } elseif (is_scalar($root)) {
             if ($root == 0) {
-                foreach ($this->roots()->all() as $rootItem) {
-                    if ($level) {
-                        $res += $this->options($rootItem, $level - 1);
-                    } elseif (is_null($level)) {
-                        $res += $this->options($rootItem, null);
-                    }
+                $query = $this->roots();
+                if (is_callable($queryCallback)) {
+                    $query = call_user_func(
+                        $queryCallback,
+                        $query
+                    );
+                }
+                foreach ($query->all() as $rootItem) {
+                    $settings['root'] = $rootItem;
+                    $settings['level'] = (!is_null($level) ? $level - 1 : $level);
+                    $res += $this->options($settings);
                 }
             } else {
                 $modelClass = $this->owner->modelClass;
+
+                /**
+                 * @var ActiveRecord $modelClass
+                 */
                 $model = new $modelClass;
-                $root = $modelClass::find()->andWhere([$model->idAttribute => $root])->one();
+                $query = $modelClass
+                    ::find()
+                    ->andWhere(
+                        [
+                            $model->idAttribute => $root
+                        ]
+                    );
+                if (is_callable($queryCallback)) {
+                    $query = call_user_func(
+                        $queryCallback,
+                        $query
+                    );
+                }
+                $root = $query->one();
                 if ($root) {
-                    $res += $this->options($root, $level);
+                    $settings['root'] = $root;
+                    $settings['level'] = $level;
+                    $res += $this->options($settings);
                 }
                 unset($model);
             }
         }
+
         return $res;
     }
 
-    public function dataFancytree($root = 0, $level = null)
-    {
-        $data = array_values($this->prepareData2Fancytree($root, $level));
+    /**
+     * @param int $root
+     * @param null $level
+     * @param null|callable $queryCallback
+     *
+     * @return array
+     */
+    public function dataFancytree(
+        $root = 0,
+        $level = null,
+        $queryCallback = null
+    ) {
+        $data = array_values(
+            $this->prepareData2Fancytree(
+                $root,
+                $level,
+                $queryCallback
+            )
+        );
         return $this->makeData2Fancytree($data);
     }
 
-    private function prepareData2Fancytree($root = 0, $level = null)
-    {
+    /**
+     * @param int $root
+     * @param null $level
+     * @param null|callable $queryCallback
+     *
+     * @return array
+     */
+    private function prepareData2Fancytree(
+        $root = 0,
+        $level = null,
+        $queryCallback = null
+    ) {
         $res = [];
         if (is_object($root)) {
+
+            /**
+             * @var NestedSetBehavior $root
+             */
             $res[$root->{$root->idAttribute}] = [
                 'key' => $root->{$root->idAttribute},
                 'title' => $root->{$root->titleAttribute}
             ];
 
+            $query = $root->children();
+            if (is_callable($queryCallback)) {
+                $query = call_user_func(
+                    $queryCallback,
+                    $query
+                );
+            }
             if ($level) {
-                foreach ($root->children()->all() as $childRoot) {
-                    $aux = $this->prepareData2Fancytree($childRoot, $level - 1);
+                foreach ($query->all() as $childRoot) {
+                    $aux = $this->prepareData2Fancytree(
+                        $childRoot,
+                        $level - 1
+                    );
 
                     if (isset($res[$root->{$root->idAttribute}]['children']) && !empty($aux)) {
                         $res[$root->{$root->idAttribute}]['folder'] = true;
                         $res[$root->{$root->idAttribute}]['children'] += $aux;
-                        
-                    } elseif(!empty($aux)) {
+
+                    } elseif (!empty($aux)) {
                         $res[$root->{$root->idAttribute}]['folder'] = true;
                         $res[$root->{$root->idAttribute}]['children'] = $aux;
                     }
                 }
             } elseif (is_null($level)) {
-                foreach ($root->children()->all() as $childRoot) {
-                    $aux = $this->prepareData2Fancytree($childRoot, null);
+                foreach ($query->all() as $childRoot) {
+                    $aux = $this->prepareData2Fancytree(
+                        $childRoot,
+                        null
+                    );
                     if (isset($res[$root->{$root->idAttribute}]['children']) && !empty($aux)) {
                         $res[$root->{$root->idAttribute}]['folder'] = true;
                         $res[$root->{$root->idAttribute}]['children'] += $aux;
-                        
-                    } elseif(!empty($aux)) {
+
+                    } elseif (!empty($aux)) {
                         $res[$root->{$root->idAttribute}]['folder'] = true;
                         $res[$root->{$root->idAttribute}]['children'] = $aux;
                     }
@@ -115,19 +275,50 @@ class NestedSetQueryBehavior extends Behavior
             }
         } elseif (is_scalar($root)) {
             if ($root == 0) {
-                foreach ($this->roots()->all() as $rootItem) {
+                $query = $this->roots();
+                if (is_callable($queryCallback)) {
+                    $query = call_user_func(
+                        $queryCallback,
+                        $query
+                    );
+                }
+                foreach ($query->all() as $rootItem) {
                     if ($level) {
-                        $res += $this->prepareData2Fancytree($rootItem, $level - 1);
+                        $res += $this->prepareData2Fancytree(
+                            $rootItem,
+                            $level - 1,
+                            $queryCallback
+                        );
                     } elseif (is_null($level)) {
-                        $res += $this->prepareData2Fancytree($rootItem, null);
+                        $res += $this->prepareData2Fancytree(
+                            $rootItem,
+                            null,
+                            $queryCallback
+                        );
                     }
                 }
             } else {
                 $modelClass = $this->owner->modelClass;
+
+                /**
+                 * @var ActiveRecord $modelClass
+                 */
                 $model = new $modelClass;
-                $root = $modelClass::find()->andWhere([$model->idAttribute => $root])->one();
+                $query = $modelClass::find()
+                                    ->andWhere([$model->idAttribute => $root]);
+                if (is_callable($queryCallback)) {
+                    $query = call_user_func(
+                        $queryCallback,
+                        $query
+                    );
+                }
+                $root = $query->one();
                 if ($root) {
-                    $res += $this->prepareData2Fancytree($root, $level);
+                    $res += $this->prepareData2Fancytree(
+                        $root,
+                        $level,
+                        $queryCallback
+                    );
                 }
                 unset($model);
             }
@@ -135,6 +326,11 @@ class NestedSetQueryBehavior extends Behavior
         return $res;
     }
 
+    /**
+     * @param array $data
+     *
+     * @return array
+     */
     private function makeData2Fancytree(&$data)
     {
         $tree = [];
